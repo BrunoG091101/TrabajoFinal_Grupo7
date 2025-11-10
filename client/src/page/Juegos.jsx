908c69dcec2f8cc564a96e1331ee6f470917b78b
FerchuTM <108917202+FerchuTM@users.noreply.github.com>
2025-11-10 16:31:11 -0300

    FloresJoseFernando
    Se actualizo Juegos.js y se agrego  juegos.css

--- client/src/page/Juegos.jsx
+++ client/src/page/Juegos.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import '../assets/css/Juegos.css'
 
 function Juegos() {
     const [juegoActual, setJuegoActual] = useState("");
@@ -15,41 +16,45 @@ function Juegos() {
     };
 
     return(
-        <div style={{ textAlign: "center", marginTop: "2rem" }}>
-            <div className="contenedorJuegos">
+        <div className="contPrincipalJuegos" style={{ textAlign: "center", marginTop: "2rem" }}>
+            <div className="contJuegosIngles">
                 <h1> Diagnóstico Inglés </h1>
 
-                <Button
-                onClick={() => ingresarJuego("primerJuego")}
-                >
-                    Juego 1
-                </Button>
+                <div className="botonesJuegos">
+                    <Button
+                    onClick={() => ingresarJuego("primerJuego")}
+                    >
+                        Juego 1
+                    </Button>
 
-                <Button
-                onClick={() => ingresarJuego("segundoJuego")}
-                >
-                    Juego 2
-                </Button>
+                    <Button
+                    onClick={() => ingresarJuego("segundoJuego")}
+                    >
+                        Juego 2
+                    </Button>
 
-                <Button
-                onClick={() => ingresarJuego("tercerJuego")}
-                >
-                    Juego 3
-                </Button>
+                    <Button
+                    onClick={() => ingresarJuego("tercerJuego")}
+                    >
+                        Juego 3
+                    </Button>
 
-                <Button
-                onClick={() => ingresarJuego("cuartoJuego")}
-                >
-                    Juego 4
-                </Button>
+                    <Button
+                    onClick={() => ingresarJuego("cuartoJuego")}
+                    >
+                        Juego 4
+                    </Button>
+                </div>
             </div>
 
-            <h1> Juego Extra </h1>
-            <Button
-            onClick={() => ingresarJuego("juegoEstrella")}
-            >
-                Atrapa las estrellas
-            </Button>
+            <div className="contJuegoExtra">
+                <h1> Juego Extra </h1>
+                <Button
+                onClick={() => ingresarJuego("juegoEstrella")}
+                >
+                    Atrapa las estrellas
+                </Button>
+            </div>
         </div>
     )
 }
